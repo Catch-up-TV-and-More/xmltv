5289bde3eb5d41a112eda6c02f8bda18ea248705
joaopa00 <jeremielapuree@yahoo.fr>
2023-03-24 22:20:03 -1000

    Auto update TV guides (10/08/2025 22:11:46-UTC)

---
+++ scripts/channels.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+
+COUNTRIES_CHANNELS = {
+    'fr': {
+        'dst': 'tv_guide_fr{}.xml',
+        'channels': [
+            'C4.api.telerama.fr',
+            'C34.api.telerama.fr',
+            'C47.api.telerama.fr',
+            'C64.api.telerama.fr',  # Equidia
+            'C78.api.telerama.fr',
+            'C80.api.telerama.fr',
+            'C110.api.telerama.fr',
+            'C111.api.telerama.fr',
+            'C112.api.telerama.fr',
+            'C118.api.telerama.fr',
+            'C119.api.telerama.fr',
+            'C140.api.telerama.fr',
+            'C174.api.telerama.fr',
+            'C182.api.telerama.fr',
+            'C183.api.telerama.fr',
+            'C184.api.telerama.fr',
+            'C192.api.telerama.fr',
+            'C195.api.telerama.fr',
+            'C205.api.telerama.fr',
+            'C226.api.telerama.fr',
+            'C234.api.telerama.fr',
+            'C240.api.telerama.fr',
+            'C245.api.telerama.fr',
+            'C260.api.telerama.fr',
+            'C273.api.telerama.fr',
+            'C295.api.telerama.fr',
+            'C328.api.telerama.fr',
+            'C329.api.telerama.fr',
+            'C421.api.telerama.fr',
+            'C444.api.telerama.fr',
+            'C445.api.telerama.fr',
+            'C446.api.telerama.fr',
+            'C458.api.telerama.fr',
+            'C459.api.telerama.fr',
+            'C481.api.telerama.fr',
+            'C482.api.telerama.fr',
+            'C491.api.telerama.fr',
+            'C529.api.telerama.fr',
+            'C535.api.telerama.fr',
+            'C539.api.telerama.fr',
+            'C540.api.telerama.fr',
+            'C1045.api.telerama.fr',
+            'C1073.api.telerama.fr',
+            'C1399.api.telerama.fr',
+            'C1400.api.telerama.fr',
+            'C1401.api.telerama.fr',
+            'C1402.api.telerama.fr',
+            'C1403.api.telerama.fr',
+            'C1404.api.telerama.fr',
+            'C1921.api.telerama.fr',
+            'C1922.api.telerama.fr',
+            'C1923.api.telerama.fr',
+            'C1924.api.telerama.fr',
+            'C1925.api.telerama.fr',
+            'C1926.api.telerama.fr',
+            'C1927.api.telerama.fr',
+            'C1928.api.telerama.fr',
+            'C1929.api.telerama.fr',
+            'C1931.api.telerama.fr',
+            'C1932.api.telerama.fr',
+            'C1933.api.telerama.fr',
+            'C1934.api.telerama.fr',
+            'C1935.api.telerama.fr',
+            'C1936.api.telerama.fr',
+            'C1937.api.telerama.fr',
+            'C1938.api.telerama.fr',
+            'C1939.api.telerama.fr',
+            'C1940.api.telerama.fr',
+            'C1941.api.telerama.fr',
+            'C1942.api.telerama.fr',
+            'C1943.api.telerama.fr',
+            'C1944.api.telerama.fr',
+            'C2111.api.telerama.fr',
+            'C3163.api.telerama.fr'  # Culturebox
+        ]
+    },
+    'fr_tnt': {
+        'dst': 'tv_guide_fr_tnt{}.xml',
+        'channels': [
+            'C192.api.telerama.fr',  # TF1
+            'C4.api.telerama.fr',  # France 2
+            'C80.api.telerama.fr',  # France 3
+            'C34.api.telerama.fr',  # Canal+
+            'C47.api.telerama.fr',  # France 5
+            'C118.api.telerama.fr',  # M6
+            'C111.api.telerama.fr',  # Arte
+            'C445.api.telerama.fr',  # C8
+            'C119.api.telerama.fr',  # W9
+            'C195.api.telerama.fr',  # TMC
+            'C446.api.telerama.fr',  # TFX
+            'C444.api.telerama.fr',  # NRJ 12
+            'C234.api.telerama.fr',  # LCP
+            'C78.api.telerama.fr',  # France 4
+            'C481.api.telerama.fr',  # BFM TV
+            'C226.api.telerama.fr',  # CNEWS
+            'C458.api.telerama.fr',  # CSTAR
+            'C482.api.telerama.fr',  # Gulli
+            'C3163.api.telerama.fr',  # Culturebox
+            'C1404.api.telerama.fr',  # TF1 Séries Films
+            'C1401.api.telerama.fr',  # L'équipe
+            'C1403.api.telerama.fr',  # 6ter
+            'C1402.api.telerama.fr',  # RMC Story
+            'C1400.api.telerama.fr',  # RMC Découverte
+            'C1399.api.telerama.fr',  # Chérie 25
+            'C112.api.telerama.fr',  # LCI
+            'C2111.api.telerama.fr'  # Franceinfo
+        ]
+    },
+    'be': {
+        'dst': 'tv_guide_be{}.xml',
+        'channels': [
+            'C23.api.telerama.fr',
+            'C50.api.telerama.fr',
+            'C164.api.telerama.fr',
+            'C168.api.telerama.fr',
+            'C187.api.telerama.fr',
+            'C377.api.telerama.fr',
+            'C892.api.telerama.fr',
+            'C1280.api.telerama.fr'
+        ]
+    },
+    'it': {
+        'dst': 'tv_guide_it{}.xml',
+        'channels': [
+            'www.la7.it',  # La7
+            'www.raiuno.rai.it',  # Rai 1
+            'www.raidue.rai.it',  # Rai 2
+            'www.raitre.rai.it',  # Rai 3
+            'rai4.raisat.it',  # Rai 4
+            'rai5.rai.it',  # Rai 5
+            'raimovie.rai.it',  # Rai Movie
+            'yoyo.raisat.it',  # Yoyo
+            'raigulp.rai.it',  # Rai Gulp
+            'raistoria.rai.it',  # Rai Storia
+            'raiscuola.rai.it',  # Rai Scuola
+        ]
+    },
+    'uk': {
+        'dst': 'tv_guide_uk{}.xml',
+        'channels': [
+            '178.tvguide.co.uk',  # STV Central
+            '185.tvguide.co.uk',  # Magic
+            '182.tvguide.co.uk',  # Kiss TV
+            '257.tvguide.co.uk',  # Sky News
+            '267.tvguide.co.uk',  # Box Hits
+            '279.tvguide.co.uk',  # The Box
+            '320.tvguide.co.uk',  # Yesterday
+            '422.tvguide.co.uk',  # Really
+            '432.tvguide.co.uk',  # Dave
+            '454.tvguide.co.uk',  # DMAX
+            '559.tvguide.co.uk',  # Quest
+            '596.tvguide.co.uk',  # Food Network
+            '871.tvguide.co.uk',  # Drama
+            '1013.tvguide.co.uk',  # Blaze
+            '1014.tvguide.co.uk',  # Quest Red
+            '1154.tvguide.co.uk',  # Birmingham TV
+            '1157.tvguide.co.uk',  # Bristol TV
+            '1171.tvguide.co.uk',  # Cardiff TV
+            '1207.tvguide.co.uk',  # Kerrang!
+            '1210.tvguide.co.uk',  # Leeds TV
+            '1212.tvguide.co.uk',  # Liverpool TV
+            '1255.tvguide.co.uk'  # Tyne & Wear TV
+        ]
+    },
+    'ch': {
+        'dst': 'tv_guide_ch{}.xml',
+        'channels': [
+            'C59.api.telerama.fr',  # SRF 1
+            'C200.api.telerama.fr',  # RSI La 1
+            'C201.api.telerama.fr',  # RSI La 2
+            'C183.api.telerama.fr',  # RTS Deux
+            'C202.api.telerama.fr'  # RTS Un
+        ]
+    },
+    'wo': {
+        'dst': 'tv_guide_wo{}.xml',
+        'channels': [
+            '140.tvguide.co.uk'  # EuroNews UK
+            '853.tvguide.co.uk'  # RT UK
+            '1183.tvguide.co.uk'  # France 24 UK
+            'C61.api.telerama.fr',  # DW UK
+            'C111.api.telerama.fr',  # Arte FR
+            'C205.api.telerama.fr'  # TV5 Monde
+            'C529.api.telerama.fr',  # France 24 FR
+            'euronews.rai.it'  # EuroNews IT
+        ]
+    }
+}
