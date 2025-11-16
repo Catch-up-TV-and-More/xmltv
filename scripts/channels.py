eb76c33163a0fdd5cd33ea61ed3dfaa780c3681b
joaopa00 <jeremielapuree@yahoo.fr>
2025-11-16 21:25:27 +0100

    Update to master (20/11/2025 22:12:07-UTC)
    Includes all changes committed via patch files.

--- scripts/channels.py
+++ scripts/channels.py
@@ -7,6 +7,7 @@ COUNTRIES_CHANNELS = {
             'C4.api.telerama.fr',
             'C34.api.telerama.fr',
             'C47.api.telerama.fr',
+            'C64.api.telerama.fr',  # Equidia
             'C78.api.telerama.fr',
             'C80.api.telerama.fr',
             'C110.api.telerama.fr',
@@ -27,8 +28,11 @@ COUNTRIES_CHANNELS = {
             'C240.api.telerama.fr',
             'C245.api.telerama.fr',
             'C260.api.telerama.fr',
+            'C273.api.telerama.fr',
+            'C295.api.telerama.fr',
             'C328.api.telerama.fr',
             'C329.api.telerama.fr',
+            'C421.api.telerama.fr',
             'C444.api.telerama.fr',
             'C445.api.telerama.fr',
             'C446.api.telerama.fr',
@@ -36,7 +40,11 @@ COUNTRIES_CHANNELS = {
             'C459.api.telerama.fr',
             'C481.api.telerama.fr',
             'C482.api.telerama.fr',
+            'C491.api.telerama.fr',
             'C529.api.telerama.fr',
+            'C535.api.telerama.fr',
+            'C539.api.telerama.fr',
+            'C540.api.telerama.fr',
             'C1045.api.telerama.fr',
             'C1073.api.telerama.fr',
             'C1399.api.telerama.fr',
@@ -68,7 +76,10 @@ COUNTRIES_CHANNELS = {
             'C1942.api.telerama.fr',
             'C1943.api.telerama.fr',
             'C1944.api.telerama.fr',
-            'C2111.api.telerama.fr'
+            'C2111.api.telerama.fr',
+            'C3163.api.telerama.fr',  # Culturebox
+            'C4138.api.telerama.fr',  # NOVO19
+            'C4139.api.telerama.fr',  # T18
         ]
     },
     'fr_tnt': {
@@ -77,29 +88,29 @@ COUNTRIES_CHANNELS = {
             'C192.api.telerama.fr',  # TF1
             'C4.api.telerama.fr',  # France 2
             'C80.api.telerama.fr',  # France 3
-            'C34.api.telerama.fr',  # Canal+
+            'C78.api.telerama.fr',  # France 4
             'C47.api.telerama.fr',  # France 5
             'C118.api.telerama.fr',  # M6
             'C111.api.telerama.fr',  # Arte
-            'C445.api.telerama.fr',  # C8
+            'C234.api.telerama.fr',  # LCP
             'C119.api.telerama.fr',  # W9
             'C195.api.telerama.fr',  # TMC
             'C446.api.telerama.fr',  # TFX
-            'C444.api.telerama.fr',  # NRJ 12
-            'C234.api.telerama.fr',  # LCP
-            'C78.api.telerama.fr',  # France 4
+            'C482.api.telerama.fr',  # Gulli
             'C481.api.telerama.fr',  # BFM TV
             'C226.api.telerama.fr',  # CNEWS
+            'C112.api.telerama.fr',  # LCI
+            'C2111.api.telerama.fr',  # Franceinfo
             'C458.api.telerama.fr',  # CSTAR
-            'C482.api.telerama.fr',  # Gulli
+            'C4139.api.telerama.fr',  # T18
+            'C4138.api.telerama.fr',  # NOVO19
             'C1404.api.telerama.fr',  # TF1 Séries Films
             'C1401.api.telerama.fr',  # L'équipe
             'C1403.api.telerama.fr',  # 6ter
             'C1402.api.telerama.fr',  # RMC Story
             'C1400.api.telerama.fr',  # RMC Découverte
             'C1399.api.telerama.fr',  # Chérie 25
-            'C112.api.telerama.fr',  # LCI
-            'C2111.api.telerama.fr'  # Franceinfo
+            'C3163.api.telerama.fr',  # Culturebox
         ]
     },
     'be': {
@@ -118,20 +129,68 @@ COUNTRIES_CHANNELS = {
     'it': {
         'dst': 'tv_guide_it{}.xml',
         'channels': [
-            'www.raiuno.rai.it'
+            'www.la7.it',  # La7
+            'www.raiuno.rai.it',  # Rai 1
+            'www.raidue.rai.it',  # Rai 2
+            'www.raitre.rai.it',  # Rai 3
+            'rai4.raisat.it',  # Rai 4
+            'rai5.rai.it',  # Rai 5
+            'raimovie.rai.it',  # Rai Movie
+            'yoyo.raisat.it',  # Yoyo
+            'raigulp.rai.it',  # Rai Gulp
+            'raistoria.rai.it',  # Rai Storia
+            'raiscuola.rai.it',  # Rai Scuola
         ]
     },
-    'en': {
-        'dst': 'tv_guide_en{}.xml',
+    'uk': {
+        'dst': 'tv_guide_uk{}.xml',
         'channels': [
-            '74.tvguide.co.uk'
+            '4.freeview.co.uk',  # Channel 4
+            '5.freeview.co.uk',  # Channel 5
+            '12.freeview.co.uk',  # Quest
+            '13.freeview.co.uk',  # E4
+            '14.freeview.co.uk',  # Film 4
+            '17.freeview.co.uk',  # Really
+            '18.freeview.co.uk',  # More 4
+            '19.freeview.co.uk',  # U&Dave
+            '20.freeview.co.uk',  # U&Drama
+            '21.freeview.co.uk',  # 5 USA
+            '27.freeview.co.uk',  # U&Yesterday
+            '32.freeview.co.uk',  # 5 Star
+            '33.freeview.co.uk',  # 5 Action
+            '35.freeview.co.uk',  # STV+1
+            '39.freeview.co.uk',  # DMAX
+            '40.freeview.co.uk',  # Quest Red
+            '43.freeview.co.uk',  # Food Network
+            '44.freeview.co.uk',  # HGTV
+            '46.freeview.co.uk',  # 5 Select
+            '49.freeview.co.uk',  # 4seven
+            '64.freeview.co.uk',  # Blaze
+            '103.freeview.co.uk',  # STV HD
+            '233.freeview.co.uk'  # Sky News
+        ]
+    },
+    'ch': {
+        'dst': 'tv_guide_ch{}.xml',
+        'channels': [
+            'C59.api.telerama.fr',  # SRF 1
+            'C200.api.telerama.fr',  # RSI La 1
+            'C201.api.telerama.fr',  # RSI La 2
+            'C183.api.telerama.fr',  # RTS Deux
+            'C202.api.telerama.fr'  # RTS Un
         ]
     },
     'wo': {
         'dst': 'tv_guide_wo{}.xml',
         'channels': [
+            '140.tvguide.co.uk'  # EuroNews UK
+            '853.tvguide.co.uk'  # RT UK
+            '1183.tvguide.co.uk'  # France 24 UK
+            'C61.api.telerama.fr',  # DW UK
             'C111.api.telerama.fr',  # Arte FR
             'C205.api.telerama.fr'  # TV5 Monde
+            'C529.api.telerama.fr',  # France 24 FR
+            'euronews.rai.it'  # EuroNews IT
         ]
     }
 }
